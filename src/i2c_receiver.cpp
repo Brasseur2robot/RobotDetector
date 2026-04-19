cae81b2822ed4a0f4ed381a7ff6a0cb0494ed358
yesman <yes@yes.com>
2026-04-19 10:29:53 +0200

    Work in progress

--- src/i2c_receiver.cpp
+++ src/i2c_receiver.cpp
@@ -1,24 +1,40 @@
+#include <Arduino.h>
 #include <Wire.h>
 
 #define I2C_SLAVE_ADDRESS 0x08
 
+void receiveEvent(int numBytes);
+void requestEvent();
+
 void setup() {
   Serial.begin(115200);
+  delay(2000);
 
   Wire.begin(I2C_SLAVE_ADDRESS);
   Wire.onReceive(receiveEvent);
-  Wire.onRequest(requestEvent);
+  // Wire.onRequest(requestEvent);
 
   Serial.println("I2C Slave Ready");
 }
 
-void loop() { delay(100); }
-
-void receiveEvent(int numBytes) {
-  while (Wire.available()) {
-    char c = Wire.read();
-    Serial.print(c);
-  }
+void loop() { 
+  delay(100); 
 }
 
-void requestEvent() { Wire.write("OK"); }
+void receiveEvent(int numBytes) {
+    if (numBytes == 5) {
+      uint8_t cmd = Wire.read();
+
+      uint16_t distance = (Wire.read() << 8) | Wire.read();
+      uint16_t angle    = (Wire.read() << 8) | Wire.read();
+
+      // Serial.print("CMD: ");
+      // Serial.print(cmd);
+      Serial.print("Distance: ");
+      Serial.print(distance);
+      Serial.print(" | Angle: ");
+      Serial.println(angle);
+    }
+}
+
+// void requestEvent() { Wire.write("OK"); }
